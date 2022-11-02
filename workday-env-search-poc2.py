e048b36beef997d33f0f6656f697d7905fff2446
dipankarTechInc <dipankar14@gmail.com>
2022-11-02 12:43:32 -0700

    2nd poc

---
+++ workday-env-search-poc2.py
@@ -0,0 +1,312 @@
+from airflow import DAG
+from airflow.operators.bash  import BashOperator
+from airflow.operators.python import PythonOperator
+from airflow.providers.mysql.operators.mysql import MySqlOperator
+from airflow.models import Variable
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+
+from airflow.hooks.base_hook import BaseHook
+from airflow.contrib.operators.slack_webhook_operator import SlackWebhookOperator
+
+import pandas as pd
+import datetime
+import pendulum
+import os
+
+FILENAME_ASH_PROD='tenantDetails_30-ash-prod.csv'
+FILENAME_ATL_NPRD='tenantDetails_30-atl-nprd.csv'
+FILENAME_DUB_NPRD='tenantDetails_30-dub-nprd.csv'
+FILENAME_DUB_PROD='tenantDetails_30-dub-prod.csv'
+FILENAME_PDX_NPRD='tenantDetails_30-pdx-nprd.csv'
+FILENAME_PDX_PROD='tenantDetails_30-pdx-prod.csv'
+
+SLACK_CONN_ID = 'slack'
+DAG_NAME = "workday-env-search-poc"
+SCHEDULE_INTERVAL = '0 4 * * *'
+
+def dag_success_callback(context):
+    slack_conn_id = "slack"
+    slack_webhook_token = BaseHook.get_connection(slack_conn_id).password
+    log_url = context.get("task_instance").log_url
+    slack_msg = f"""
+            :airflow-new: DAG has succeeded.
+            *Task*: {context.get('task_instance').task_id}
+            *Dag*: {context.get('task_instance').dag_id}
+            *Execution Time*: {context.get('execution_date')}
+            <{log_url}| *Log URL*>
+            """
+    slack_alert = SlackWebhookOperator(
+        task_id="slack_test",
+        http_conn_id="slack",
+        webhook_token=slack_webhook_token,
+        message=slack_msg,
+        username="airflow",
+    )
+    return slack_alert.execute(context=context)
+
+
+#start
+def send_slack_notification(message='', attachments=None, channel=None):
+    """Send message to Slack.
+    message: Text of the message to send. See below for an explanation of
+    formatting. This field is usually required, unless you're providing only
+    attachments instead. Provide no more than 40,000 characters or risk truncati
+    attachments: [list of max 100] dict[s] slack message attachment[s]
+    see https://api.slack.com/docs/message-attachments#attachment_structure
+    channel:  a channel in your workspace starting with `#`
+    """
+    assert isinstance(message, str) and message or attachments
+    if isinstance(attachments, dict):
+        attachments = [attachments]
+
+    slack_webhook_token = BaseHook.get_connection(SLACK_CONN_ID).password
+    print('Slack webhook failed....')
+    print(slack_webhook_token)
+
+    notification_operator = SlackWebhookOperator(
+        task_id='slack_failed',
+        http_conn_id=SLACK_CONN_ID,
+        webhook_token=slack_webhook_token,
+        icon_url='https://github.com/apache/airflow/raw/v1-10-stable/airflow/www/static/pin_100.png',
+        message=message,
+        attachments=attachments
+    )
+    return notification_operator.execute(attachments)
+
+def failed_task_slack_notification(kwargs):
+    """Send failed task notification with context provided by operator."""
+    print(kwargs['ti'])
+    dag = kwargs['ti'].dag_id
+    run_id = kwargs['run_id']
+    task = kwargs['ti'].task_id
+    exec_date = kwargs['execution_date']
+    try_number = kwargs['ti'].try_number - 1
+    max_tries = kwargs['ti'].max_tries + 1
+    exception = kwargs['exception']
+    log_url = kwargs['ti'].log_url
+    # command = kwargs['ti'].command(),
+
+    message = (
+        f'`DAG`  {dag}'
+        f'\n`Run Id`  {run_id}'
+        f'\n`Task`  {task} _(try {try_number} of {max_tries})_'
+        f'\n`Execution`  {exec_date}'
+        f'\n```{exception}```'
+        # f'`Command`  {command}\n'
+    )
+
+    attachments = {
+        'mrkdwn_in': ['text', 'pretext'],
+        'pretext': ':red_circle: *Task Failed*',
+        'title': 'airflow-web.wheely-dev.com',
+        'title_link': f'https://airflow-web.wheely-dev.com',
+        'text': message,
+        'actions': [
+            {
+                'type': 'button',
+                'name': 'view log',
+                'text': 'View log :airflow:',
+                'url': log_url,
+                'style': 'primary',
+            },
+        ],
+        'color': 'danger',  # 'good', 'warning', 'danger', or hex ('#439FE0')
+        'fallback': 'details',  # Required plain-text summary of the attachment
+    }
+
+    send_slack_notification(attachments=attachments)
+#end
+
+
+def download_from_s3(key: str, bucket_name: str, local_path: str) -> str:
+    hook = S3Hook('s3_conn')
+    file_name = hook.download_file(key=key, bucket_name=bucket_name, local_path=local_path)
+    return file_name
+
+def download_from_s3_ash_prod(key: str, bucket_name: str, local_path: str) -> str:
+    hook = S3Hook('s3_conn')
+    file_name = hook.download_file(key=key, bucket_name=bucket_name, local_path=local_path)
+    return file_name
+
+def download_from_s3_atl_nprd(key: str, bucket_name: str, local_path: str) -> str:
+    hook = S3Hook('s3_conn')
+    file_name = hook.download_file(key=key, bucket_name=bucket_name, local_path=local_path)
+    return file_name
+
+def download_from_s3_dub_nprd(key: str, bucket_name: str, local_path: str) -> str:
+    hook = S3Hook('s3_conn')
+    file_name = hook.download_file(key=key, bucket_name=bucket_name, local_path=local_path)
+    return file_name
+
+def download_from_s3_dub_prod(key: str, bucket_name: str, local_path: str) -> str:
+    hook = S3Hook('s3_conn')
+    file_name = hook.download_file(key=key, bucket_name=bucket_name, local_path=local_path)
+    return file_name
+
+def download_from_s3_pdx_nprd(key: str, bucket_name: str, local_path: str) -> str:
+    hook = S3Hook('s3_conn')
+    file_name = hook.download_file(key=key, bucket_name=bucket_name, local_path=local_path)
+    return file_name
+
+def download_from_s3_pdx_prod(key: str, bucket_name: str, local_path: str) -> str:
+    hook = S3Hook('s3_conn')
+    file_name = hook.download_file(key=key, bucket_name=bucket_name, local_path=local_path)
+    return file_name
+
+def rename_file(ti, new_name: str, task_id: str) -> None:
+    downloaded_file_name = ti.xcom_pull(task_ids=[task_id])
+    downloaded_file_path = '/'.join(downloaded_file_name[0].split('/')[:-1])
+    os.rename(src=downloaded_file_name[0], dst=f"{downloaded_file_path}/{new_name}")
+
+default_args = {
+    'owner': 'airflow',
+    'retries': 0,
+    'on_success_callback': dag_success_callback,
+    'on_failure_callback': failed_task_slack_notification
+}
+
+with DAG(
+    dag_id='workday_env_search_poc2',
+    default_args=default_args,
+    schedule_interval='@daily',
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+    dagrun_timeout=datetime.timedelta(minutes=60)
+) as dag:
+
+    task_download_from_s3_ash_prod = PythonOperator(
+        task_id='download_from_s3_ash_prod',
+        python_callable=download_from_s3,
+        op_kwargs={
+            'key': 'minerva/airflow-data/ash-prod-tenantDetails_30.csv',
+            'bucket_name': Variable.get("S3_BUCKET"),
+            'local_path': '/opt/airflow/data/'
+        }
+    )
+
+    # Rename the file
+    task_rename_file_ash_prod = PythonOperator(
+        task_id='rename_file_ash_prod',
+        python_callable=rename_file,
+        op_kwargs={
+            'new_name': FILENAME_ASH_PROD,
+            'task_id': 'download_from_s3_ash_prod'
+        }
+    )
+
+    task_download_from_s3_atl_nprd = PythonOperator(
+        task_id='download_from_s3_atl_nprd',
+        python_callable=download_from_s3,
+        op_kwargs={
+            'key': 'minerva/airflow-data/atl-nprd-tenantDetails_30.csv',
+            'bucket_name': Variable.get("S3_BUCKET"),
+            'local_path': '/opt/airflow/data/'
+        }
+    )
+
+    # Rename the file
+    task_rename_file_atl_nprd = PythonOperator(
+        task_id='rename_file_atl_nprd',
+        python_callable=rename_file,
+        op_kwargs={
+            'new_name': FILENAME_ATL_NPRD,
+            'task_id': 'download_from_s3_atl_nprd'
+        }
+    )
+
+    task_download_from_s3_dub_nprd = PythonOperator(
+        task_id='download_from_s3_dub_nprd',
+        python_callable=download_from_s3,
+        op_kwargs={
+            'key': 'minerva/airflow-data/dub-nprd-tenantDetails_30.csv',
+            'bucket_name': Variable.get("S3_BUCKET"),
+            'local_path': '/opt/airflow/data/'
+        }
+    )
+
+    # Rename the file
+    task_rename_file_dub_nprd = PythonOperator(
+        task_id='rename_file_dub_nprd',
+        python_callable=rename_file,
+        op_kwargs={
+            'new_name': FILENAME_DUB_NPRD, 
+            'task_id': 'download_from_s3_dub_nprd'
+        }
+    )
+
+    task_download_from_s3_dub_prod = PythonOperator(
+        task_id='download_from_s3_dub_prod',
+        python_callable=download_from_s3,
+        op_kwargs={
+            'key': 'minerva/airflow-data/dub-prod-tenantDetails_30.csv',
+            'bucket_name': Variable.get("S3_BUCKET"),
+            'local_path': '/opt/airflow/data/'
+        }
+    )
+
+    # Rename the file
+    task_rename_file_dub_prod = PythonOperator(
+        task_id='rename_file_dub_prod',
+        python_callable=rename_file,
+        op_kwargs={
+            'new_name': FILENAME_DUB_PROD,
+            'task_id': 'download_from_s3_dub_prod'
+        }
+    )
+
+    task_download_from_s3_pdx_nprd = PythonOperator(
+        task_id='download_from_s3_pdx_nprd',
+        python_callable=download_from_s3,
+        op_kwargs={
+            'key': 'minerva/airflow-data/pdx-nprd-tenantDetails_30.csv',
+            'bucket_name': Variable.get("S3_BUCKET"),
+            'local_path': '/opt/airflow/data/'
+        }
+    )
+
+    # Rename the file
+    task_rename_file_pdx_nprd = PythonOperator(
+        task_id='rename_file_pdx_nprd',
+        python_callable=rename_file,
+        op_kwargs={
+            'new_name': FILENAME_PDX_NPRD,
+            'task_id': 'download_from_s3_pdx_nprd'
+        }
+    )
+
+    task_download_from_s3_pdx_prod = PythonOperator(
+        task_id='download_from_s3_pdx_prod',
+        python_callable=download_from_s3,
+        op_kwargs={
+            'key': 'minerva/airflow-data/pdx-prod-tenantDetails_30.csv',
+            'bucket_name': Variable.get("S3_BUCKET"),
+            'local_path': '/opt/airflow/data/'
+        }
+    )
+
+    # Rename the file
+    task_rename_file_pdx_prod = PythonOperator(
+        task_id='rename_file_pdx_prod',
+        python_callable=rename_file,
+        op_kwargs={
+            'new_name': FILENAME_PDX_PROD,
+            'task_id': 'download_from_s3_pdx_prod'
+        }
+    )
+
+    load_wta_env_tenants_to_db = MySqlOperator(
+        mysql_conn_id='mysql_conn',
+        task_id='load_wta_env_tenants_to_db',
+        sql='WTA_ENV_TENANTS_insert.sql',
+        dag=dag
+    )
+
+    task_download_from_s3_ash_prod >> task_rename_file_ash_prod >> load_wta_env_tenants_to_db
+    task_download_from_s3_atl_nprd >> task_rename_file_atl_nprd >> load_wta_env_tenants_to_db
+    task_download_from_s3_dub_nprd >> task_rename_file_dub_nprd >> load_wta_env_tenants_to_db
+    task_download_from_s3_dub_prod >> task_rename_file_dub_prod >> load_wta_env_tenants_to_db
+    task_download_from_s3_pdx_nprd >> task_rename_file_pdx_nprd >> load_wta_env_tenants_to_db
+    task_download_from_s3_pdx_prod >> task_rename_file_pdx_prod >> load_wta_env_tenants_to_db
+  
+if __name__ == "__main__":
+    dag.cli()
